97503646627c55eb23d1c6cb7de1b05e51d07f77
Monica398 <mjaen2112@gmail.com>
2026-06-16 23:14:01 -0600

    Armar Ramos js

---
+++ js/armar_Ramo.js
@@ -0,0 +1,242 @@
+
+let flores = [];
+let extras = [];
+
+
+const contenedorFlores = document.getElementById("contenedorFlores");
+const listaResumen = document.getElementById("listaResumen");
+const subtotalFlores = document.getElementById("subtotalFlores");
+const totalGeneral = document.getElementById("totalGeneral");
+const totalExtrasTexto = document.getElementById("totalExtras");
+const checkTarjeta = document.getElementById("tarjeta");
+const checkChocolates = document.getElementById("chocolates");
+
+
+checkTarjeta.addEventListener("change", actualizarResumen);
+checkChocolates.addEventListener("change", actualizarResumen);
+// Traemos los filtros del HTML
+const filtroTipo = document.getElementById("filtroTipo");
+const filtroColor = document.getElementById("filtroColor");
+filtroTipo.addEventListener("change", mostrarFlores);
+filtroColor.addEventListener("change", mostrarFlores);
+const costoEntrega = 2500;
+
+
+// Esta función muestra las flores en la página
+function mostrarFlores() {
+
+    
+    let contenido = "";
+
+    
+    for (const flor of flores) {
+
+        if (filtroTipo.value !== "Todas" && flor.nombre !== filtroTipo.value) {
+            continue;
+        }
+
+        
+        let tieneColor = false;
+
+        
+        for (const color of flor.colores) {
+
+            
+            if (color === filtroColor.value) {
+                tieneColor = true;
+            }
+        }
+
+        if (filtroColor.value !== "Todos" && tieneColor === false) {
+            continue;
+        }
+
+        let botonesColores = "";
+
+        for (const color of flor.colores) {
+            botonesColores += `
+                <button class="color-${color}" onclick="seleccionarColor(${flor.id}, '${color}')">
+                    ${color}
+                </button>
+            `;
+        }
+        contenido += `
+            <div class="tarjeta-flor">
+
+                <img src="${flor.imagen}" alt="${flor.nombre}">
+
+                <h4>${flor.nombre}</h4>
+
+                <p class="precio">₡${flor.precio} c/u</p>
+
+                <p class="descripcion-flor">${flor.descripcion}</p>
+
+                <p class="texto-colores">Colores disponibles:</p>
+
+                <div class="colores">
+                    ${botonesColores}
+                </div>
+
+                <div class="cantidad">
+                    <button onclick="restarCantidad(${flor.id})">-</button>
+                    <span>${flor.cantidad}</span>
+                    <button onclick="sumarCantidad(${flor.id})">+</button>
+                </div>
+
+            </div>
+        `;
+    }
+
+    contenedorFlores.innerHTML = contenido;
+}
+
+
+// aumenta la cantidad de una flor
+function sumarCantidad(id) {
+
+    for (const flor of flores) {
+
+        if (flor.id === id) {
+
+            
+            flor.cantidad++;
+        }
+    }
+
+    
+    mostrarFlores();
+
+    
+    actualizarResumen();
+}
+
+function restarCantidad(id) {
+
+    
+    for (const flor of flores) {
+
+        
+        if (flor.id === id && flor.cantidad > 0) {
+
+            
+            flor.cantidad--;
+        }
+    }
+
+    
+    mostrarFlores();
+
+    
+    actualizarResumen();
+}
+
+
+// guarda el color que el usuario seleccionó para una flor
+function seleccionarColor(id, color) {
+
+    for (const flor of flores) {
+
+        
+        if (flor.id === id) {
+
+            
+            flor.colorSeleccionado = color;
+        }
+    }
+
+    
+    mostrarFlores();
+
+    
+    actualizarResumen();
+}
+
+
+// actualiza el resumen del pedido
+function actualizarResumen() {
+
+    
+    let contenidoResumen = "";
+    let subtotal = 0;
+    let totalExtras = 0;
+
+    if (checkTarjeta.checked) {
+        totalExtras += 1500;
+    }
+
+    
+    if (checkChocolates.checked) {
+        totalExtras += 3000;
+    }
+
+    for (const flor of flores) {
+        if (flor.cantidad > 0) {
+
+            
+            let totalFlor = flor.precio * flor.cantidad;
+
+            
+            subtotal = subtotal + totalFlor;
+
+            
+            contenidoResumen += `
+                <div class="item-resumen">
+                    <p>
+                        ${flor.nombre} x${flor.cantidad}<br>
+                        Color: ${flor.colorSeleccionado}
+                    </p>
+
+                    <strong>
+                        ₡${totalFlor}
+                    </strong>
+                </div>
+            `;
+        }
+    }
+
+    if (contenidoResumen === "") {
+        listaResumen.innerHTML = `
+            <p class="mensaje-vacio">
+                Aún no has agregado flores.
+            </p>
+        `;
+    } else {
+
+        
+        listaResumen.innerHTML = contenidoResumen;
+    }
+
+    
+    subtotalFlores.innerHTML = "₡" + subtotal;
+
+    
+    totalExtrasTexto.innerHTML = "₡" + totalExtras;
+
+    
+    totalGeneral.innerHTML = "₡" + (subtotal + totalExtras + costoEntrega);
+}
+
+/*======================
+PARTE QUE TENEMOS QUE REVISAR CON RESPESCTO A UNIR EL JSON CON EL JS,TENTATIVA
+=======================*/
+// Esta parte carga las flores desde el archivo JSON
+// sirve para leer el archivo flores.json
+fetch("data/flores.json")
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+
+        // Guardamos las flores del JSON dentro del arreglo flores
+        flores = datos.flores;
+
+        // A cada flor le agregamos cantidad y color seleccionado
+        for (const flor of flores) {
+            flor.cantidad = 0;
+            flor.colorSeleccionado = "";
+        }
+
+        // Mostramos las flores en la página
+        mostrarFlores();
+
+        // Mostramos el resumen inicial
+        actualizarResumen();
+    });
